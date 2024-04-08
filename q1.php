de2ae11e2aa0f8246e7b06695f56e9b889024523
Sailesh aryal <sailesharyal500@gmail.com>
2024-04-08 08:11:06 +0545

    lerning some basic of php

---
+++ q1.php
@@ -0,0 +1,26 @@
+<html?>
+<!-- this is a comment in html -->
+<link rel ="stylesheet" href = "q1.css">
+<head>
+   <title>My first php program </title>
+ 
+
+
+
+</head>
+
+<body>
+
+<h1>hello world</h1>
+
+<?php
+
+echo("Hello World ");
+
+?>
+</body>
+
+
+
+
+</html>
---
+++ q2.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>learning php</title>
+</head>
+<body>
+
+ <h1?> this is code to make a varibale </h1>
+ <br>
+
+    <?php
+
+        $var1 = "Sailesh aryal";
+        $var2  = "Kathmandu";
+
+
+        echo "<h1>".$var1."</h1>";
+        echo "<br>";
+        echo $var2;
+        echo "<br>";
+
+    # this is comment in php 
+    # ressaign different variable in php 
+
+
+    $var1 = "sunway Collage";
+
+    $var2 = "Bhaktapur";
+
+     echo $var1; 
+     echo "<br>";
+     echo $var2;
+     echo "<br>";
+
+
+    ?>
+
+
+<h1>
+    <?php 
+        
+            echo $var1;
+            echo "<br">
+            echo $var2;
+
+       
+       
+    ?>
+</h1>
+</body>
+</html>
